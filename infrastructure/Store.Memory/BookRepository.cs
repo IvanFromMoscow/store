c44b2358726fd07bc92ddd503ffae4fb5c793926
IvanFromMoscow <ibusygin1988@gmail.com>
2021-09-20 16:03:31 +0300

    Добавить UI в виде MVC для поиска книги

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new Book[] {
+            new Book(1, "C# 9 and .NET 5 – Modern Cross-Platform Development: Build intelligent apps, websites, and services with Blazor, ASP.NET Core, and Entity Framework Core using Visual Studio Code, 5th Edition"),
+            new Book(2, "Hands-On Unity 2021 Game Development: Create, customize, and optimize your own professional games from scratch with Unity 2021, 2nd Edition 2nd ed. Edition"),
+            new Book(3, "Professional C# and .NET 2021st Edition")
+        };
+        public Book[] GetAllBookByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository booksRepository;
+
+        public SearchController(IBookRepository booksRepository)
+        {
+            this.booksRepository = booksRepository;
+        }
+        public IActionResult Index(string query)
+        {
+            var books = booksRepository.GetAllBookByTitle(query);
+            return View(books);
+        }
+    }
+}
