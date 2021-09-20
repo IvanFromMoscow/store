9375f66e1140f9af7e5c32f19341bb3c5ece9221
IvanFromMoscow <ibusygin1988@gmail.com>
2021-09-20 15:21:07 +0300

    Внести изменения в интерфейс репозитория

---
+++ domain/Store/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public interface IBookRepository
+    {
+        Book GetById(int id);
+        Book[] GetAllBookByTitle(string titlePart);
+    }
+}
